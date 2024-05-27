0ccf9be2eb173d8e2f4af741da3632425e2e3cf6
Teguh Satriya <teguh.satriya.w@gmail.com>
2024-05-27 20:52:29 +0700

    Add project files.

---
+++ FileEncryption/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileEncryption;
+
+Services.Encrypt("H:/EventAttendees.sql", "H:/EventAttendees.sql.enc");
+
+Services.Decrypt("H:/EventAttendees.sql.enc", "H:/EventAttendees.sql.dec");
+
+Console.WriteLine("Hello, World!");
---
+++ WebUrl/Services.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace WebUrl
+{
+    public static class Services
+    {
+        public static string GetURL(Process process)
+        {
+            if (process == null)
+                throw new ArgumentNullException("process");
+
+            if (process.MainWindowHandle == IntPtr.Zero)
+                return null;
+
+            //AutomationElement elm = AutomationElement.FromHandle(process.MainWindowHandle);
+            //if (elm == null)
+            //    return null;
+
+            return string.Empty;
+        }
+    }
+}
