86c413dc1bc1cad119c75c2c95e8c2dcd252c135
Teguh Satriya <teguh.satriya.w@gmail.com>
2024-06-24 08:13:37 +0700

    active windows

---
+++ ActiveWindow/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows.Automation;
+
+namespace ActiveWindows
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Automation.AddAutomationFocusChangedEventHandler(OnFocusChanged);
+
+            Console.ReadLine();
+            Automation.RemoveAllEventHandlers();
+        }
+
+        private static AutomationElement? _lastWindow;
+        private static void OnFocusChanged(object sender, AutomationFocusChangedEventArgs e)
+        {
+            AutomationElement? elementFocused = sender as AutomationElement;
+            if (elementFocused == null)
+                return;
+
+            try
+            {
+                AutomationElement? topLevelWindow = GetParentWindow(elementFocused);
+                if (topLevelWindow == null)
+                    return;
+
+                if (topLevelWindow != _lastWindow)
+                {
+                    _lastWindow = topLevelWindow;
+                    Console.WriteLine("Focus moved to window: {0}", topLevelWindow.Current.Name);
+                }
+            }
+            catch (ElementNotAvailableException)
+            {
+            }
+        }
+
+        private static AutomationElement? GetParentWindow(AutomationElement element)
+        {
+            AutomationElement node = element;
+            try
+            {
+                while (!Equals(node.Current.ControlType, ControlType.Window))
+                {
+                    node = TreeWalker.ControlViewWalker.GetParent(node);
+                    if (node == null)
+                        return null;
+                }
+
+                return node;
+            }
+            catch (ElementNotAvailableException)
+            {
+            }
+
+            return null;
+        }
+    }
+}
