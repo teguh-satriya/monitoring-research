7f248be7912f2eb220d1536ce360df5d1f12b50c
Teguh Satriya <teguh.satriya.w@gmail.com>
2024-07-05 06:22:59 +0700

    add system info

--- InstalledApp/Program.cs
+++ InstalledApp/Program.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.Win32;
+using System.Globalization;
 
 string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
-using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key))
+using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key))
 {
     foreach (string subkey_name in key.GetSubKeyNames())
     {
         using (RegistryKey subkey = key.OpenSubKey(subkey_name))
         {
-            Console.WriteLine(subkey.GetValue("DisplayName"));
+            if (subkey.GetValue("DisplayName") != null) 
+            {
+                DateTime? dt = null;
+                if (subkey.GetValue("InstallDate") != null)
+                {
+                    dt = DateTime.ParseExact(subkey.GetValue("InstallDate").ToString(), "yyyyMMdd", CultureInfo.InvariantCulture);
+                }
+
+                string display = $"App : {subkey.GetValue("DisplayName")} | Version : {subkey.GetValue("DisplayVersion")} | Publisher : {subkey.GetValue("Publisher")} | Installed Date : {dt?.ToString("yyyy-MM-dd")}";
+
+                Console.WriteLine(display);
+            }
+            
         }
     }
 }
---
+++ SystemInfo/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System.Management;
+
+namespace SystemInfo 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            getOperatingSystemInfo();
+            getProcessorInfo();
+            Console.ReadLine();
+        }
+
+        public static void getOperatingSystemInfo()
+        {
+            Console.WriteLine("Displaying operating system info....\n");
+
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+            foreach (ManagementObject managementObject in mos.Get())
+            {
+                if (managementObject["Caption"] != null)
+                {
+                    Console.WriteLine("Operating System Name  :  " + managementObject["Caption"].ToString()); 
+                }
+                if (managementObject["OSArchitecture"] != null)
+                {
+                    Console.WriteLine("Operating System Architecture  :  " + managementObject["OSArchitecture"].ToString());
+                }
+                if (managementObject["CSDVersion"] != null)
+                {
+                    Console.WriteLine("Operating System Service Pack   :  " + managementObject["CSDVersion"].ToString());
+                }
+            }
+        }
+
+        public static void getProcessorInfo()
+        {
+            Console.WriteLine("\n\nDisplaying HW Info....");
+            RegistryKey processor_name = Registry.LocalMachine.OpenSubKey(@"Hardware\Description\System\CentralProcessor\0", RegistryKeyPermissionCheck.ReadSubTree);
+
+            if (processor_name != null)
+            {
+                if (processor_name.GetValue("ProcessorNameString") != null)
+                {
+                    Console.WriteLine(processor_name.GetValue("ProcessorNameString"));
+                }
+            }
+
+            var memoryInfo = GC.GetGCMemoryInfo();
+           
+            Console.WriteLine($"Total Memory: {memoryInfo.TotalAvailableMemoryBytes / (1024.0 * 1024.0)} MB");
+
+            using (var searcher = new ManagementObjectSearcher("select * from Win32_VideoController"))
+            {
+                Console.WriteLine("\nDisplaying GPU Info....");
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    Console.WriteLine("Name: " + obj["Name"]);
+                    Console.WriteLine("Adapter RAM: " + obj["AdapterRAM"]);
+                    Console.WriteLine("Driver Version: " + obj["DriverVersion"]);
+                    Console.WriteLine("Video Processor: " + obj["VideoProcessor"]);
+                    Console.WriteLine("Video Architecture: " + obj["VideoArchitecture"]);
+                }
+            }
+        }
+    }
+}
